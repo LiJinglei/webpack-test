ec97e6ced5efe8265ff9b9a5e53abe8b322f525b
李景磊 <lijlei@yonyou.com>
2019-08-13 08:57:31 +0800

    添加输出路径

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+const path = require('path');
 module.exports = {
     entry: './src/script/main.js',
     output: {
-        path: './dist/js',
+        path: path.join(__dirname, 'dist'),
         filename: "bundle.js"
     }
 }
