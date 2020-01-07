c30192fbe1db6223f34dcae1e4b7b90e2e6b6ed1
lijlei@yonyou.com <muzijinglei@163.com>
2020-01-07 23:30:23 +0800

    webpack   outer

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+/**
+ * Created by 李景磊 on 2019/7/23.
+ */
+function hello() {
+    // alert('hello world');
+    console.log('hello world');
+}
+hello();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 通过 npm 安装
+
 module.exports = {
     entry: './src/script/main.js',
     output: {
-        path: './dist/js',
+        path: path.resolve(__dirname, 'dist/js'),
         filename: "bundle.js"
-    }
+    },
+    module: {
+        rules: [
+            {test: /.txt$/, use: 'raw-loader'},
+            {test: /.css$/, use: 'css-loader'},
+        ]
+    }, plugins: [
+        new HtmlWebpackPlugin({template: './index.html'})
+    ]
 }
