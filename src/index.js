86ab206c265eb5ed48a79f0a56231c2143ad60ac
muzijinglei@163.com <muzijinglei@163.com>
2020-11-06 21:00:39 +0800

    test code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 /**
  * Created by 李景磊 on 2020/10/27.
  */
+import helloworld from './js/main'
+
+helloworld();
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // 通过 npm 安装
 
 module.exports = {
-    entry: './src/script/main.js',
+    entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
+        path: path.resolve(__dirname, 'dist/'),
         filename: "bundle.js"
     },
     module: {
         rules: [
             {test: /.txt$/, use: 'raw-loader'},
             {test: /.css$/, use: 'css-loader'},
+            {
+                test: /\.(js|jsx)$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'src')
+            }
         ]
     }, plugins: [
         new HtmlWebpackPlugin({template: './index.html'})
-    ]
+    ],
+    mode: 'production'
 }
